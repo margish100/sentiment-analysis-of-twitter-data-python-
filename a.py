ce7ae458faba729c7ae2c115c8828540dbf063ef
margish <41039995+margish100@users.noreply.github.com>
2019-03-17 19:05:20 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,9 @@
+import.re
+drf. valid mail(email)
+if ln (email)>7
+    if rematch ()
+        return true
+    return false
+if valid  enmail addeess
+    print("this is valid emil address")
+print ("this is not  valid emaail address ")
